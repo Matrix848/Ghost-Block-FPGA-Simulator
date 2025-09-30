c4e405b29237929a304ce876f7bf806d56b766ae
Matrix848 <89012816+Matrix848@users.noreply.github.com>
2025-09-30 17:01:28 +0200

    Removed test code from Main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,4 @@
 mod fpga;
 
 fn main() {
-    //let args: Vec<String> = env::args().collect();
-    //dbg!(args);
-
-    let mut i = 0;
-    let mut dir: i8 = 1;
-
-    let width = 5;
-    let height = 10;
-
-    let mut j = 0;
-    for _ in 0..height * (width) {
-        println!("{}, {}", i, j);
-
-        if (i == width - 1 && dir == 1) || (i == 0 && dir == -1) {
-            println!("switch");
-            dir *= -1;
-            j += 1;
-        } else {
-            i = (i as isize + dir as isize) as usize;
-        }
-    }
 }
